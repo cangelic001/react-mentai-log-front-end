fcf800026c8fe66ad68e3a747840265e40a2f8b4
cangelic001 <ica.christina992@gmail.com>
2025-03-03 23:36:09 +0800

    style: adjust landing

--- src/components/CarouselLanding/CarouselLanding.jsx
+++ src/components/CarouselLanding/CarouselLanding.jsx
@@ -1,6 +1,4 @@
 import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
-import CardBasic from '../CardBasic/CardBasic';
 import './CarouselLanding.css';
 
 function CarouselLanding() {
@@ -25,8 +23,11 @@ function CarouselLanding() {
             <Carousel className="carousel-container text-center p-4">
                 {textData.map((info, index) => (
                     <Carousel.Item key={index}>
-                        <h5 style={{color: "#ffd972"}}>{info.title}</h5>
-                        <h6 style={{color: "#fae3a5"}}>{info.text}</h6>
+                        <div className="carousel-box">
+                            <h5 style={{ color: "#ffa50a" }}>{info.title}</h5>
+                            <h6 style={{ color: "#ff94a6" }}>{info.text}</h6>
+                        </div>
+                        
                     </Carousel.Item>
                 ))}
             </Carousel>
